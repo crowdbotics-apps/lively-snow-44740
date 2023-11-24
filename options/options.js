cc0797da3af3aba100f1d47279b71f2c4ac3f484
Crowdbotics <team@crowdbotics.com>
2023-11-24 13:29:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-snow-44740",
+  url: "https://lively-snow-44740.botics.co",
+  api: "https://lively-snow-44740.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
